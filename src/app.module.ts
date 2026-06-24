a9df83d68a444055f508d2540932799e0c65413b
maheshDhananjaya <dhananjayam94@gmail.com>
2026-06-24 19:53:48 +0530

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ProjectModule } from './project/project.module';
+import { TaskModule } from './task/task.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
@@ -10,6 +13,9 @@ import { AuthModule } from './auth/auth.module';
     }),
     PrismaModule,
     AuthModule,
+    ProjectModule,
+    TaskModule,
+    CommentModule,
   ],
   controllers: [],
   providers: [],
